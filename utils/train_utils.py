99079d1828a815529191db08b87a6686320a2e9e
Shaurya Sehgal <56615472+0shaurya@users.noreply.github.com>
2025-12-17 23:47:47 -0600

    add relative error

--- utils/train_utils.py
+++ utils/train_utils.py
@@ -1,7 +1,7 @@
 import torch
 from torch import nn
 import torch.optim as optim
-device = torch.device('cuda' if torch.cuda.is_available() else 'mps' if torch.bakends.mps.is_available() else 'cpu')
+device = torch.device('cuda' if torch.cuda.is_available() else 'mps' if torch.backends.mps.is_available() else 'cpu')
 
 class PQModel(nn.Module):
     def __init__(self, d):
@@ -15,28 +15,38 @@ class PQModel(nn.Module):
         return res.squeeze()
 
 def train_model(Ainvn, x, y, d, lr=0.001, num_epochs=1000):
-	model = PQModel(d).to(device)
-	optimizer = optim.AdamW(model.parameters(), lr=lr)
-	model.train()
+    model = PQModel(d).to(device)
+    optimizer = optim.AdamW(model.parameters(), lr=lr)
+    model.train()
 
-	for epoch in range(num_epochs):
-		optimizer.zero_grad()
-		y_pred = model(Ainvn, x)
-		loss = nn.MSELoss()(y_pred, y)
-		loss.backward()
-		optimizer.step()
+    for epoch in range(num_epochs):
+        optimizer.zero_grad()
+        y_pred = model(Ainvn, x)
+        
+        loss = nn.MSELoss()(y_pred, y)
+        # loss = torch.norm(y_pred - y) / torch.norm(y)  # relative L2 error
 
-		if epoch % 100 == 0:
-			print(f"Epoch {epoch}/{num_epochs}, MSE Loss = {loss.item()}")
+        loss.backward()
+        optimizer.step()
 
-	return model
+        if epoch % 100 == 0:
+            print(f"Epoch {epoch}/{num_epochs}, MSE Loss = {loss.item()}")
+
+    return model
 
 
-def eval_model(model, Ainv, x, y):
-    model.eval()
-    y_pred = model(Ainv, x)
-    mse = nn.MSELoss()(y_pred, y).item()
-    return mse
+def eval_model(model, Ainv, x, y, type='relative'):
+    if type == 'relative':
+        model.eval()
+        y_pred = model(Ainv, x)
+        abs_error = torch.norm(y_pred - y, p=2).item()
+        rel_error = abs_error / torch.norm(y, p=2).item()
+        return rel_error
+    elif type == 'mse':
+        model.eval()
+        y_pred = model(Ainv, x)
+        mse = nn.MSELoss()(y_pred, y).item()
+        return mse
 
 # Ay = x
 # Ainv @ x = y
